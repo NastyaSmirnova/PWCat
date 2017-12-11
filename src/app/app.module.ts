f931015f9591d052efd34c5db5a02218f269a5a4
nsmirnova <smirnovanastya1602@gmail.com>
2017-12-11 11:23:59 +0200

    Add service worker script to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
 import { 
   MatToolbarModule,
   MatCardModule,
@@ -20,7 +23,8 @@ import { ImgCardComponent } from './img-card/img-card.component';
     BrowserModule,
     MatToolbarModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : []
   ],
   providers: [],
   bootstrap: [AppComponent]
